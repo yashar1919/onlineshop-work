6eecdbde28a4d0d72a384e1bfea3bc309deb3e8f
yashar <yashar.talebi@gmail.com>
2022-08-26 13:04:23 +0430

    Upgrade react-router-dom from v5 to v6

--- src/App.js
+++ src/App.js
@@ -3,28 +3,19 @@ import Store from './components/Store';   //Component
 import Navbar from './components/Navbar'; //Component
 import ProductContextProvider from './context/ProductContextProvider';  //Context
 import CartContextProvider from './context/CartContextProvider';  //Context
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import ShopCart from './components/ShopCart';
 
 function App() {
   return (
-    /*     <ProductContextProvider>
-          <CartContextProvider>
-            <Navbar />
-            <Store />
-          </CartContextProvider>
-        </ProductContextProvider> */
-
-
-
     <ProductContextProvider>
       <CartContextProvider>
         <Navbar />
-        <Switch>
-          <Route path="/products" component={Store} />
-          <Route path="/cart" component={ShopCart} />
-          <Redirect to="/products" />
-        </Switch>
+        <Routes>
+          <Route path="/products" element={<Store />} />
+          <Route path="/cart" element={<ShopCart />} />
+          <Route path="/*" element={<Navigate to="/products" />} />
+        </Routes>
       </CartContextProvider>
     </ProductContextProvider>
   );
